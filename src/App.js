ec9ea524639863fa94fbc1a6b96bf9b72d7c0831
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-19 20:22:20 +0530

    :sparkles: Render all images in the doodles folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    doodles: []
+  }
+
+  componentWillMount(){
+    let doodles = []
+
+    for(let i = 1; i <= 19; i++){
+        doodles[i] = `./assets/doodles/${i}.jpg`
+    }
+    this.setState({doodles})
+  }
+
   render() {
+    const { doodles } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="row">
+            {doodles.map(doodle => (
+              <div className="col-md-6 col-sm-12">   
+                <div className="thumbnail">           
+                  <img key={doodle} src={require(`${doodle}`)} alt=""/>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
